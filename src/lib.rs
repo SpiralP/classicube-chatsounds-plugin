74d7e229f5ae794073d43d5a08cdcc3aa7ae65d2
SpiralP <spiral.palmer@gmail.com>
2019-10-06 00:36:06 -0700

    pretty alright

--- src/lib.rs
+++ src/lib.rs
@@ -4,18 +4,22 @@ use classicube::{
 };
 use lazy_static::lazy_static;
 use std::{
-  ffi::CString,
   os::raw::{c_int, c_void},
   ptr,
   sync::Mutex,
-};
-use winapi::{
-  shared::ntdef::NULL,
-  um::winuser::{MessageBoxA, MB_ICONINFORMATION, MB_OK},
+  thread,
 };
 
 lazy_static! {
   static ref CHATSOUNDS: Mutex<Option<Chatsounds>> = Mutex::new(None);
+}
+
+fn print<T: Into<String>>(s: T) {
+  let s = s.into();
+  unsafe {
+    let cc_str = classicube::String::from_string(s);
+    Chat_Add(&cc_str);
+  };
 }
 
 extern "C" fn chat_on_received(_obj: *mut c_void, msg: *const classicube::String, msg_type: c_int) {
@@ -28,20 +32,34 @@ extern "C" fn chat_on_received(_obj: *mut c_void, msg: *const classicube::String
     unsafe { *msg }
   };
 
-  let msg = msg.to_string();
+  let mut msg = msg.to_string();
 
-  if msg.contains("bap") {
-    let s = String::from("ag bo");
-    unsafe {
-      let cc_str = classicube::String::from_string(s);
-      Chat_Add(&cc_str);
-    };
+  if let Some(pos) = msg.rfind("&f") {
+    let text = msg.split_off(pos + 2);
 
-    if let Some(chatsounds) = CHATSOUNDS.lock().unwrap().as_mut() {
-      let mut found = chatsounds.find("bap");
-      let mut loaded = found[0].download();
-      loaded.play(&chatsounds.device);
-    }
+    // use winapi::um::winuser::{MessageBoxA, MB_ICONINFORMATION, MB_OK};
+
+    // let lp_text = std::ffi::CString::new(format!("{}", msg)).unwrap();
+    // let lp_caption = std::ffi::CString::new(format!("{}", text)).unwrap();
+
+    // unsafe {
+    //   MessageBoxA(
+    //     std::ptr::null_mut(),
+    //     lp_text.as_ptr(),
+    //     lp_caption.as_ptr(),
+    //     MB_OK | MB_ICONINFORMATION,
+    //   );
+    // }
+    thread::spawn(move || {
+      if let Some(chatsounds) = CHATSOUNDS.lock().unwrap().as_mut() {
+        let text = text.trim();
+        let mut found = chatsounds.find(text);
+        if !found.is_empty() {
+          let mut loaded = found[0].download();
+          loaded.play(&chatsounds.sink);
+        }
+      }
+    });
   }
 }
 
@@ -54,10 +72,18 @@ extern "C" fn init() {
     );
   }
 
-  let chatsounds = Chatsounds::new(
-    "Metastruct/garrysmod-chatsounds",
-    "sound/chatsounds/autoadd",
-  );
+  let chatsounds = Chatsounds::new(vec![
+    (
+      "Metastruct/garrysmod-chatsounds".to_string(),
+      "sound/chatsounds/autoadd".to_string(),
+    ),
+    (
+      "PAC3-Server/chatsounds".to_string(),
+      "sounds/chatsounds".to_string(),
+    ),
+  ]);
+
+  chatsounds.sink.set_volume(0.1);
 
   *CHATSOUNDS.lock().unwrap() = Some(chatsounds);
 }
