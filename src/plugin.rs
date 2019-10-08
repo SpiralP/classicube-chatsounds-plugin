455482c5e8166fd2bb2059a678381b85a71a85c1
SpiralP <spiral.palmer@gmail.com>
2019-10-08 14:57:41 -0700

    actually it should go in lib

--- src/plugin.rs
+++ src/plugin.rs
@@ -1,11 +1,6 @@
 use crate::{command, events, option, printer::PRINTER};
 use classicube::{ScheduledTask, Server};
 use detour::static_detour;
-use std::cell::Cell;
-
-thread_local! {
-  static LOADED: Cell<bool> = Cell::new(false);
-}
 
 static_detour! {
   static TICK_DETOUR: unsafe extern "C" fn(*mut ScheduledTask);
@@ -21,39 +16,27 @@ fn tick_detour(task: *mut ScheduledTask) {
 }
 
 pub fn load() {
-  let loaded = LOADED.with(|a| a.get());
+  events::load();
+  command::load();
 
-  if !loaded {
-    events::load();
-    command::load();
+  option::load();
 
-    option::load();
-
-    unsafe {
-      if let Some(tick_original) = Server.Tick {
-        TICK_DETOUR.initialize(tick_original, tick_detour).unwrap();
-        TICK_DETOUR.enable().unwrap();
-      }
+  unsafe {
+    if let Some(tick_original) = Server.Tick {
+      TICK_DETOUR.initialize(tick_original, tick_detour).unwrap();
+      TICK_DETOUR.enable().unwrap();
     }
-
-    crate::chatsounds::load();
-
-    LOADED.with(|a| a.set(true));
   }
+
+  crate::chatsounds::load();
 }
 
 pub fn unload() {
-  let loaded = LOADED.with(|a| a.get());
+  events::unload();
 
-  if loaded {
-    events::unload();
-
-    unsafe {
-      TICK_DETOUR.disable().unwrap();
-    }
-
-    crate::chatsounds::unload();
+  unsafe {
+    TICK_DETOUR.disable().unwrap();
   }
 
-  LOADED.with(|a| a.set(false));
+  crate::chatsounds::unload();
 }
