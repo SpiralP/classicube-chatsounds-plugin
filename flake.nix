94aff74e585bec4fb1f336f8d04beb3ccbe05d4b
SpiralP <spiral.palmer@gmail.com>
2023-07-01 22:08:51 -0500

    update nix flake

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
   };
 
   outputs = { nixpkgs, ... }:
@@ -36,11 +36,23 @@
                   ]
                 );
             };
-            cargoSha256 = "sha256-BsKoyHPRVgrkzoQL5ls82UNLyHYPqjJ6pXIH1HKJt5s=";
+
+            cargoLock = {
+              lockFile = ./Cargo.lock;
+              outputHashes = {
+                "async-dispatcher-0.1.0" = "sha256-rqpQ176/PnI9vvPrwQvK3GJbryjb3hHkb+o1RyCZ3Vg=";
+                "chatsounds-0.2.0" = "sha256-HJq5MXkXnEKGOHX+DRzVhQjLTPmar0MWW7aItqrlpys=";
+                "classicube-helpers-2.0.0+classicube.1.3.5" = "sha256-E9ORHAO8rGVCMXTq2TvsQwrSV5H5WF3bAj5+OJ2f7jA=";
+                "classicube-sys-2.0.0+classicube.1.3.5" = "sha256-VXHyJwF8cdX3PlTn7xgbviGg3D5bsRRh375I+DRpE4g=";
+                "color-backtrace-0.3.0" = "sha256-wVf6EEmD/PqHGJtVUXBg5y2kXPXxGtQTU52WurrFv+M=";
+              };
+            };
+
             nativeBuildInputs = with pkgs; [
               pkg-config
               rustPlatform.bindgenHook
             ];
+
             buildInputs = with pkgs; [
               alsa-lib
               at-spi2-atk
