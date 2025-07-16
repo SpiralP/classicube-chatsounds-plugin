7bc83a5a627c2f049722bc0ddad7a4df3072da11
SpiralP <spiral.palmer@gmail.com>
2025-07-16 12:36:59 -0700

    fix: only append app name once

--- src/modules/app_name.rs
+++ src/modules/app_name.rs
@@ -1,4 +1,4 @@
-use std::ffi::CString;
+use std::{cell::Cell, ffi::CString};
 
 use classicube_sys::{Server, String_AppendConst};
 
@@ -16,6 +16,14 @@ impl AppNameModule {
 
 impl Module for AppNameModule {
     fn load(&mut self) {
+        thread_local!(
+            static APPENDED: Cell<bool> = const { Cell::new(false) };
+        );
+
+        if APPENDED.get() {
+            return;
+        }
+
         let append_app_name = CString::new(format!(" cs{}", env!("CARGO_PKG_VERSION"))).unwrap();
 
         let c_str = append_app_name.as_ptr();
@@ -24,6 +32,8 @@ impl Module for AppNameModule {
         unsafe {
             String_AppendConst(&raw mut Server.AppName, c_str);
         }
+
+        APPENDED.set(true);
     }
 
     fn unload(&mut self) {}
